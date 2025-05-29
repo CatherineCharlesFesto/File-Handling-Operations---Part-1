442b640a61c63b8ddfd28b0468ca2820fd7cea64
Catherine <catherinecharlesfesto@gmail.com>
2025-05-29 19:46:58 +0300

    my

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-file=open('Text1.txt')
+file=open('Text1.txt','r')
 print(file.read())
-file.close
+file.close()
+
+filewrite=open('Text1.txt','w')
+filewrite.write("I AM LEARNING TO ADD MORE DATA TO THE EXISTING FILE")
+filewrite.write("I am adding a new a new file to the existing file")
+filewrite.close()
+
+fileappend=open('Text1.txt','a')
+fileappend.write("I am runig")
+fileappend.close
